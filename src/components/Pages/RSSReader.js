70c1c51387c60ebf9f9a59974e3740d9b5da997a
Tom Spaulding <tom.spaulding@realart.com>
2017-01-27 13:42:04 -0500

    Digg RSS Reader added

--- src/components/Pages/RSSReader.js
+++ src/components/Pages/RSSReader.js
@@ -1,11 +1,43 @@
 var React = require('react');
 
 var RssReader = React.createClass({
-    render: function(){
-        return(
-            <div>RSS Reader</div>
+    setReaderIframeHeight: function(){
+        var windowHeight = $(window).height();
+        var navBarHeight = $(".navbar").height();
+
+        var remainingHeight = windowHeight - navBarHeight;
+        $(".rssReaderContainer").height(remainingHeight);
+    },
+    startWindowDetectionResize: function(){
+        $(window).resize(this.setReaderIframeHeight());
+    },
+    componentDidMount: function(){
+        this.setReaderIframeHeight();
+        this.startWindowDetectionResize();
+    },
+    render: function () {
+        return (
+            <div className="rssReaderContainer" style={styles.container}>
+                <iframe style={styles.iframe} src="https://digg.com/reader"></iframe>
+            </div>
         )
     }
 });
 
 module.exports = RssReader;
+
+var styles = {
+    container:{
+        width: '100%',
+        display: 'flex',
+        marginTop: '-21px',
+        overflow: 'hidden'
+    },
+    iframe: {
+        width: '100%',
+        height: '100%',
+        overflow: 'hidden',
+        flexGrow: '1',
+        overflow: 'hidden'
+    }
+};
