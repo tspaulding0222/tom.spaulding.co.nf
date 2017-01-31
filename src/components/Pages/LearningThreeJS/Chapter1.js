9cb06838b887aa439a67456b7454cc944a03ac3e
Tom Spaulding <tom.spaulding@realart.com>
2017-01-31 15:20:01 -0500

    Finished Chapter 1 of Three.JS Book

--- src/components/Pages/LearningThreeJS/Chapter1.js
+++ src/components/Pages/LearningThreeJS/Chapter1.js
@@ -1,8 +1,20 @@
 var React = require('react');
 
 var ThreeJsPlayground = React.createClass({
+    initStat: function(){
+        var stats = new Stats();
+        stats.setMode(0);
+        stats.domElement.style.position = 'absolute';
+        stats.domElement.style.left = '0px';
+        stats.domElement.style.top = '0px';
+
+        document.getElementById("Stats-output").appendChild(stats.domElement);
+        return stats;
+    },
     componentDidMount: function(){
         var container = document.getElementById("threeJsPlaygroundContainer");
+
+        var stats = this.initStat();
 
         var scene = new THREE.Scene();
         var camera = new THREE.PerspectiveCamera(45, container.offsetWidth/container.offsetHeight, 0.1, 1000);
@@ -10,11 +22,15 @@ var ThreeJsPlayground = React.createClass({
         var renderer = new THREE.WebGLRenderer();
         renderer.setClearColor(new THREE.Color(0xEEEEEE));
         renderer.setSize(container.offsetWidth, container.offsetHeight);
+
+        //Added to make chrome mac shadows look softer
         renderer.shadowMapType = THREE.PCFSoftShadowMap;
+        //
+
         renderer.shadowMapEnabled = true;
 
-        var axes = new THREE.AxisHelper(20);
-        scene.add(axes);
+        // var axes = new THREE.AxisHelper(20);
+        // scene.add(axes);
 
         var planeGeometry = new THREE.PlaneGeometry(60, 20, 1, 1);
         var planeMaterial = new THREE.MeshLambertMaterial({color: 0xffffff});
@@ -53,13 +69,43 @@ var ThreeJsPlayground = React.createClass({
         spotLight.position.set(-40, 60, -10);
         spotLight.castShadow = true;
         spotLight.shadowCameraVisible = true;
+
+        //Added to make chrome mac shadows look better
         spotLight.shadowMapHeight = 2048;
         spotLight.shadowMapWidth = 2048;
+        //
+
         scene.add(spotLight);
 
         container.appendChild(renderer.domElement);
-        renderer.render(scene, camera);
 
+        var controls = new function(){
+            this.rotationSpeed = 0.02;
+            this.bouncingSpeed = 0.03;
+        };
+
+        var gui = new dat.GUI({autoPlace: false});
+        gui.domElement.id = "datGui";
+        gui.add(controls, 'rotationSpeed', 0, 0.5);
+        gui.add(controls, 'bouncingSpeed', 0, 0.5);
+        document.getElementById("datGuiContainer").appendChild(gui.domElement);
+
+        var step = 0;
+        function renderScene(){
+            stats.update();
+
+            cube.rotation.x += controls.rotationSpeed;
+            cube.rotation.y += controls.rotationSpeed;
+            cube.rotation.z += controls.rotationSpeed;
+
+            step+=controls.bouncingSpeed;
+            sphere.position.x = 20 + (10 *(Math.cos(step)));
+            sphere.position.y = 2 + (10 * Math.abs(Math.sin(step)));
+
+            requestAnimationFrame(renderScene);
+            renderer.render(scene, camera);
+        }
+        renderScene();
     },
     render: function(){
         return (
@@ -71,7 +117,8 @@ var ThreeJsPlayground = React.createClass({
                 </div>
 
                 <div style={styles.threeJsPlaygroundContainer} id="threeJsPlaygroundContainer">
-
+                    <div id="Stats-output"></div>
+                    <div style={styles.datGuiPlacement} id="datGuiContainer"></div>
                 </div>
             </div>
         )
@@ -83,6 +130,12 @@ module.exports = ThreeJsPlayground;
 var styles = {
     threeJsPlaygroundContainer:{
         height: '100vh',
-        width: '100%'
+        width: '100%',
+        position: 'relative'
+    },
+    datGuiPlacement:{
+        position: 'absolute',
+        top: '0',
+        right: '0'
     }
-}
+};
